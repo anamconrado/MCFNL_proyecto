d9beb2c1e49128d4fd31724dc78cf4936205cd2b
jjgalvezviruet <jj.galvezviruet@outlook.es>
2021-05-03 19:12:48 +0200

    Se ha aadido un módulo para calcular frecuencias y con ellas los coeficientes de transmisión y reflexión

--- fdtd/2d/src/fdtd/frecuency_analysis.py
+++ fdtd/2d/src/fdtd/frecuency_analysis.py
@@ -1,36 +1,64 @@
 import numpy as np
 import copy
-
+import matplotlib.pyplot as plt
+from scipy.fft import fft, fftfreq
 from fdtd.common import X, Y, L, U
 
-class Freq_analysis:
-    def __init__(self, measures, data, data_input):
-        # Power values
-        self._measures = copy.deepcopy(measures)
-        
-        # Times
-        self._times = copy.deepcopy(data[0]["time"])
-        
-        # Ports positions
-        sourceposition = data_input["coordinates"][data_input["elements"][data_input["sources"][0]["elemId"]][0]][0]
-        self._ports = {"0": [abs(sourceposition - data_input["coordinates"][data_input["elements"][data_input["measures"]["port_inc"]["elemId"]][0]][0])],\
-            "1": [abs(sourceposition - data_input["coordinates"][data_input["elements"][data_input["measures"]["port_refl"]["elemId"]][0]][0])],\
-            "2": [abs(sourceposition - data_input["coordinates"][data_input["elements"][data_input["measures"]["port_trans"]["elemId"]][0]][0])],}
-        
-        # Port times
-        self._ports["0"].append([0,10])     # Firsts times at port 0 in ns
-        self._ports["1"].append([14,24])   # Second times at port 1 in ns
-        self._ports["2"].append([40,50])   # Firsts times at port 3 in ns
+def means2(vec):
+    lenvec = len(vec)
+    if lenvec % 2 == 0:
+        vecpar = np.array([vec[i] for i in range(0,lenvec) if i%2 == 0])
+        vecimp = np.array([vec[i] for i in range(0,lenvec) if i%2 == 1])
+    else: 
+        vecpar = np.array([vec[i] for i in range(0,lenvec-1) if i%2 == 0])
+        vecimp = np.array([vec[i] for i in range(0,lenvec) if i%2 == 1])       
+    return (vecpar + vecimp)/2
 
-        for i in self._ports:
-            self._ports[i].append([(self._times[j], self._measures.Ports(int(i))[j])\
-                for j in range(0,len(self._times)) if (self._ports[i][1][0] < self._times[j]*10**9 and self._ports[i][1][1] > self._times[j]*10**9)])
+def Fourier_trans(measures,data,data_input):
+    # Power values
+    measures = copy.deepcopy(measures)
+    
+    # Times
+    times = np.array(list(map(lambda x: x*10**9, copy.deepcopy(data[0]["time"]))))
+    Ntimes = len(times)
 
-        print("hola")
+    # Ports positions
+    sourceposition = data_input["coordinates"][data_input["elements"][data_input["sources"][0]["elemId"]][0]][0]
+    ports = {"0": [abs(sourceposition - data_input["coordinates"][data_input["elements"][data_input["measures"]["port_inc"]["elemId"]][0]][0])],\
+        "1": [abs(sourceposition - data_input["coordinates"][data_input["elements"][data_input["measures"]["port_refl"]["elemId"]][0]][0])],\
+        "2": [abs(sourceposition - data_input["coordinates"][data_input["elements"][data_input["measures"]["port_trans"]["elemId"]][0]][0])],}
+    
+    # Port times
+    ports["0"].append([0,10])     # Firsts times at port 0 in ns
+    ports["1"].append([15,25])   # Second times at port 1 in ns
+    ports["2"].append([41,51])   # Firsts times at port 3 in ns
 
-        def Fourier_trans():
-            # Calculo de frecuencias
-            timestep = self._times[1] - self._times[0]
-            for i in self._ports.values():
-                i.append((np.fft.fftfreq(len(i[2][0]))/timestep), np.fft.fft(i[2][1]) )
-        
+    for i in ports:
+        ports[i].append([[],[]]) 
+
+
+    for i in ports:
+        for j in range(0,Ntimes): 
+            if (ports[i][1][0] < times[j] and ports[i][1][1] > times[j]):
+                ports[i][2][1].append(abs(measures.Ports(int(i))[j]))
+                ports[i][2][0].append(times[j])
+            else:
+                ports[i][2][1].append(0)
+                ports[i][2][0].append(times[j])
+
+    # Nmeasures = len(ports[i][2][0])
+    Nmeasures = Ntimes
+
+    # Calculo de frecuencias y transformada de Fourier.
+    timestep = times[1] - times[0]
+    for i in ports.values():
+        frequencies = (fftfreq(Nmeasures)/timestep)[:Nmeasures//2]
+        transform = np.abs((fft(i[2][1]))[:Nmeasures//2])
+        i.append([frequencies, transform] )
+
+    # Calculo de coeficientes.
+    R = means2(ports["2"][3][1])/means2(ports["0"][3][1])
+    T = means2(ports["1"][3][1])/means2(ports["0"][3][1])
+
+    return (ports,[R,T])
+
